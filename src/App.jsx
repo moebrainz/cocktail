839b88bd899bf316fdc4f76d7f194cf246d07d1d
moebrainz <moebrainz@gmail.com>
2025-10-16 10:06:11 +0100

    implementing about section

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import gsap from "gsap";
 import { ScrollTrigger, SplitText } from "gsap/all";
+import About from "./components/About";
 import Cocktails from "./components/Cocktails";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
@@ -12,6 +13,7 @@ const App = () => {
       <Navbar />
       <Hero />
       <Cocktails />
+      <About />
     </main>
   );
 };
